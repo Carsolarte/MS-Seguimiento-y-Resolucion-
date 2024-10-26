e05354804932e85ec6d1af00e0997aa2838271dc
Carolina Solarte López <108251168+Carsolarte@users.noreply.github.com>
2024-10-25 22:05:58 -0500

    Add files via upload

---
+++ SeguimientoResolucion/src/main/java/com/devInnovators/SeguimientoResolucion/SeguimientoResolucionApplication.java
@@ -0,0 +1,13 @@
+package com.devInnovators.SeguimientoResolucion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoResolucionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientoResolucionApplication.class, args);
+	}
+
+}
