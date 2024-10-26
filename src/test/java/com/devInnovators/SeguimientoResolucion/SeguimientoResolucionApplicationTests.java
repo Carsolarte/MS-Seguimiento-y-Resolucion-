ea1885b54653369b0899ca2457d3104685572818
Carsolarte <carsolarte@unicauca.edu.co>
2024-10-25 22:08:44 -0500

    Primeros cambios

---
+++ src/test/java/com/devInnovators/SeguimientoResolucion/SeguimientoResolucionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devInnovators.SeguimientoResolucion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoResolucionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
